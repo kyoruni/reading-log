d40d6cb45412a861f21cb377a3b7edf2933bda00
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-07-20 18:07:44 +0900

    :lipstick: #2 jsonから読み込んで表示

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Button from 'primevue/button';
 import Header from './components/Header.vue';
+import BookList from '@/components/BookList.vue'
 </script>
 
 <template>
   <Header/>
   <main>
+    <BookList/>
     <Button label="Submit" />
     <i class="pi pi-check"></i>
     <i class="pi pi-times"></i>
---
+++ src/components/BookList.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import books from '@/assets/json/books.json';
+</script>
+
+<template>
+  <ul>
+    <li v-for="book in books" :key="book.id">{{ book.name }}</li>
+  </ul>
+</template>
