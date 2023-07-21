416e58b977b29b792622d8eab075cf5e8d9ed369
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-07-22 00:23:29 +0900

    :recycle: #2 リネーム

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
 import Header from './components/Header.vue';
-import BookList from '@/components/BookList.vue'
+import ContentsList from '@/components/ContentsList.vue'
 </script>
 
 <template>
   <Header/>
   <main>
-    <BookList/>
+    <ContentsList/>
   </main>
 </template>
