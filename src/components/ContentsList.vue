4b38b8034623065eb3c94acb89494fcc4a3345ce
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-07-22 00:53:06 +0900

    :wrench: #2 データテーブルを使えるようにする

--- src/components/ContentsList.vue
+++ src/components/ContentsList.vue
@@ -1,9 +1,61 @@
 <script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
 import books from '@/assets/json/books.json';
+
+interface BookData {
+  id: number;
+  title: string;
+  // publisher: string;
+  // publish_date: string;
+  // author: string;
+  // page: number;
+  // completion_date: string;
+  // category: string;
+  // impression: string;
+};
+
+interface Table {
+  title: string;
+  // align: string;
+  // sortable: boolean;
+  key: string;
+  // width: string;
+}
+
+const contents = ref<BookData[]>([]);
+const headers = ref<Table[]>([]);
+
+onBeforeMount(() => {
+  fetchItemList();
+});
+
+const fetchItemList = () => {
+  contents.value = books.slice();
+  headers.value = [
+    { title: "タイトル", key: "title" }
+    // { title: "読了日", align: 'start', sortable: true, key: "completion_date", width: "1px" },
+    // { title: "出版社", align: 'start', sortable: true, key: "publisher", width: "1px"},
+    // { title: "出版日", align: 'start', sortable: true, key: "publish_date", width: "1px" },
+    // { title: "著者", align: 'start', sortable: true, key: "author", width: "1px" },
+    // { title: "カテゴリー", align: 'start', sortable: true, key: "category", width: "100px" },
+    // { title: "ひとこと", align: 'start', sortable: true, key: "impression", width: "200px" }
+  ];
+  console.log(headers.value);
+  console.log(contents.value);
+};
 </script>
 
 <template>
-  {{ books }}
+  <v-data-table
+    class="pa-5"
+    :headers="headers"
+    :items="contents"
+    item-value="name"
+    density="compact"
+    :fixed-header="true"
+    :multi-sort="true"
+    scrollX
+  ></v-data-table>
 </template>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,18 @@ import App from './App.vue'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
+import { VDataTable } from 'vuetify/labs/VDataTable'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VDataTable
+  },
   directives,
+  theme: {
+    defaultTheme: 'light',
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
