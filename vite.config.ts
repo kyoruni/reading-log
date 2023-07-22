9c20facde1b6848d4f9e2de87683a633293f2599
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-07-23 01:41:09 +0900

    :bee: #9 もう一回

--- vite.config.ts
+++ vite.config.ts
@@ -16,21 +16,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // ビルドするとcsvが読み込めなくなるので、その対処
-  build: {
-    rollupOptions: {
-      output: {
-        // 'permissions-policy' ヘッダーを設定
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[hash].[ext]',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        intro: `<meta http-equiv="permissions-policy" content="interest-cohort=()" />`
-      }
-    }
-  }
 })
